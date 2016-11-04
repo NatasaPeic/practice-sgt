d83098b99a49f294fbccb0649fad750879c8ce4e
NatasaPeic <peic.natasa@gmail.com>
2016-11-04 01:04:17 -0400

    Add query to retreive data from database

---
+++ assets/scripts/select.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'books'
+});
+
+connection.connect();
+
+
+// no arguments are passed besides callback because there isn't a placeholder.
+connection.query('select * from books', function(err, result){
+
+  if(err){
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
